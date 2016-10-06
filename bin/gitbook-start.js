97d0f8e81ca169790c1936f7f387b5f03eebc6a5
alu0100785265 <alu0100785265@ull.edu.es>
2016-10-06 16:22:56 +0000

    cambios en la estructura del script

--- bin/gitbook-start.js
+++ bin/gitbook-start.js
@@ -1,41 +1,33 @@
+#!/usr/bin/env node
+
 var ejs = require('ejs');
-var gulp = require('gulp');
-var ncp = require("ncp");
-var read = require('fs').readFileSync;
 var fs = require ('fs-extra');
 var argv = require('minimist')(process.argv.slice(2));
-console.dir(argv.a);
+
+
+
+// expresión regular que caza con .ejs
 
 var re = /.ejs/g;
 
 
 // Creamos la carpeta
 
-fs.mkdirsSync('../'+argv.a);
+fs.mkdirsSync(argv.a);
 
-// Copiamos todos los archivos
+//Ver los nombres de los archivos dentro de las carpetas
+
+var names = fs.readdirSync('node_modules/gitbook-start-ericlucastania/template');
 
 
-/*
-ncp("../template", "../" + argv.a, function (err) {
- if (err) {
-   return console.error(err);
- }
- console.log('done!');
-});
-
-*/
-
-        
-var names = fs.readdirSync('../template');
 
 function recursive(names,folder){
     for (var i in names){
         
         if(names[i].match(re)){
         
-            //probando el render
-            var data = ejs.renderFile('../template/'+ folder + names[i],{
+            //Renderizamos el fichero
+            var data = ejs.renderFile('node_modules/gitbook-start-ericlucastania/template/'+ folder + names[i],{
                 
                 autor:{
                     name: argv.a,
@@ -45,26 +37,36 @@ function recursive(names,folder){
                     wikiurl: argv.e
                 }
                 
-            },function(err,data){if(err){console.log(err)} else{return data}});
+            },function(err,data){
+                if(err){
+                    throw err;
+                    
+                } else{
+                    return data;
+                    
+                }
+            });
             
+            
+            
+            //sustituimos el nombre, para quitarle la extensión ejs
             
             var newstr = names[i].replace(re, '');
-            console.log(newstr);
+            
+            
+            
            
-            fs.writeFile('../' + argv.a + '/' + folder + newstr, data, (err) => {
+            fs.writeFile(argv.a + '/' + folder + newstr, data, (err) => {
               if (err) throw err;
-              console.log('It\'s saved!');
             });
         }
         else{
-            fs.mkdirsSync('../' + argv.a + '/' +names[i]);
-            recursive(fs.readdirSync('../template/' + names[i]),names[i] + '/')
+            fs.mkdirsSync(argv.a + '/' +names[i]);
+            recursive(fs.readdirSync('node_modules/gitbook-start-ericlucastania/template/' + names[i]),names[i] + '/')
         }
     }
 };
 
 recursive(names,'');
-
-
 
 
